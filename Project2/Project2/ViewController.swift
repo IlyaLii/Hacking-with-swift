6cf522ae38450cca6fd4b2174c9c1d1e49a18780
Ilya <agronat88@icloud.com>
2020-01-31 19:18:45 +0300

    added alert and scoreLabel

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -19,6 +20,8 @@ class ViewController: UIViewController {
                      "nigeria", "poland", "russia",
                      "spain", "uk", "us"]
     var score = 0
+    var correctAnswer = 0
+    var countGames = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,15 +34,42 @@ class ViewController: UIViewController {
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
-
-
     }
 
     func askQuestion() {
+        countGames += 1
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+        scoreLabel.text = "Score: \(score)"
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var titleAlert : String
+        if sender.tag == correctAnswer {
+            score += 1
+            titleAlert = "Correct"
+        } else {
+            score -= 1
+            titleAlert = "Wrong! That’s the flag of \(countries[sender.tag])"
+        }
+        
+        var messageAlert = "Your score \(score)"
+        if countGames == 10 {
+            messageAlert = "Your score for 10 games: \(score)"
+            score = 0
+            countGames = 0
+        }
+        
+        let alert = UIAlertController(title: titleAlert, message: messageAlert, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(action)
+        present(alert, animated: true) {
+            self.askQuestion()
+        }
+    }
 }
 
