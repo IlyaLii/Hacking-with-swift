7d3477010e06fe3b6d7625bbb7cbc18b54250f20
Ilya <agronat88@icloud.com>
2020-01-31 14:22:04 +0300

    project2 with interface

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Li on 1/31/20.
+//  Copyright © 2020 Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
