bdd69bda7aaa9e547bef7f033fb69c8a24d0d2e6
Ilya <agronat88@icloud.com>
2020-01-31 13:02:10 +0300

    added fileManager

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var images = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                images.append(item)
+            }
+        }
     }
 
 
