d5254fffef50720a237afc333ba610fac4dac1f9
Ilya <agronat88@icloud.com>
2020-01-31 13:14:12 +0300

    added interface

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -8,10 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-    
-    var images = [String]()
+    var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,11 +19,22 @@ class ViewController: UIViewController {
         let items = try! fm.contentsOfDirectory(atPath: path)
         for item in items {
             if item.hasPrefix("nssl") {
-                images.append(item)
+                pictures.append(item)
             }
         }
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+    
 
 }
 
