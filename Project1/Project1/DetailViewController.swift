050389342e99f68cdc3a163dfafed5ca69129859
Ilya <agronat88@icloud.com>
2020-01-31 23:59:12 +0300

    added UIActivityViewController to project1

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -18,6 +18,8 @@ class DetailViewController: UIViewController {
         navigationItem.largeTitleDisplayMode = .never
         title = selectedImage
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let selectedImage = selectedImage {
             imageView.image = UIImage(named: selectedImage)
         }
@@ -32,15 +34,12 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+            else { return }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true, completion: nil)
+    }
 }
