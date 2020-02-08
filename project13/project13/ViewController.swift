69a49f4ce7fd5f70e716208c9dfdc2072a5f7fff
Ilya <agronat88@icloud.com>
2020-02-08 15:13:55 +0300

    added UIImagePicker project13

--- project13/project13/ViewController.swift
+++ project13/project13/ViewController.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var slider: UISlider!
+    var currentImage: UIImage! {
+        didSet {
+            self.imageView.image = self.currentImage
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "LOL"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPicture))
     }
 
+    @objc func addPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+        
+    }
     @IBAction func changeIntensity(_ sender: Any) {
     }
     @IBAction func changeFilter(_ sender: Any) {
