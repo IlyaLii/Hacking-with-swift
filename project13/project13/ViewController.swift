5c2631441aa3d4263901349012e099267d4aaf0d
Ilya <agronat88@icloud.com>
2020-02-08 14:58:33 +0300

    added interface project13

---
+++ project13/project13/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  project13
+//
+//  Created by Li on 2/8/20.
+//  Copyright © 2020 Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeIntensity(_ sender: Any) {
+    }
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    @IBAction func save(_ sender: Any) {
+    }
+}
+
