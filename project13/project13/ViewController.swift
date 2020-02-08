9adaa718f3e37c843fee33b3934565df4145a008
Ilya <agronat88@icloud.com>
2020-02-08 15:45:42 +0300

    added intensity func project13

--- project13/project13/ViewController.swift
+++ project13/project13/ViewController.swift
@@ -6,21 +6,23 @@
 //  Copyright © 2020 Li. All rights reserved.
 //
 
+import CoreImage
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var slider: UISlider!
-    var currentImage: UIImage! {
-        didSet {
-            self.imageView.image = self.currentImage
-        }
-    }
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "LOL"
+        title = "CISepiaTone"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPicture))
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
     @objc func addPicture() {
@@ -30,13 +32,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(picker, animated: true)
     }
     
+    func applyProcessing() {
+        currentFilter.setValue(slider.value, forKey: kCIInputIntensityKey)
+        
+        if let cgImage = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgImage)
+            imageView.image = processedImage
+        }
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
         currentImage = image
         
+        let beginImage = CIImage(image: image)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     @IBAction func changeIntensity(_ sender: Any) {
+        applyProcessing()
     }
     @IBAction func changeFilter(_ sender: Any) {
     }
