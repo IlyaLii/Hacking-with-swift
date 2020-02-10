bf95ad81a7b3cf3bd355d5530cfad54a4e3a1a65
Ilya <agronat88@icloud.com>
2020-02-10 16:25:35 +0300

    added UNActions to category  projec21

--- project21/project21/ViewController.swift
+++ project21/project21/ViewController.swift
@@ -8,7 +8,7 @@
 import UserNotifications
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +29,8 @@ class ViewController: UIViewController {
     }
 
     @objc func sheduleLocal() {
+        registerCategories()
+        
         let center = UNUserNotificationCenter.current()
         center.removeAllPendingNotificationRequests()
 
@@ -42,6 +44,45 @@ class ViewController: UIViewController {
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "Show", title: "Tell me more...", options: .foreground)
+        let remindLater = UNNotificationAction(identifier: "remind", title: "Remind me later", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, remindLater], intentIdentifiers: [], options: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["CustomData"] as? String {
+            print(customData)
+            
+            
+            var title = ""
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                print("default")
+                title = "Default"
+            case "Show":
+                print("show")
+                title = "Show"
+            case "remind":
+                title = "Remind"
+                sheduleLocal()
+            default:
+                break
+            }
+            let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            completionHandler()
+        }
     }
 }
 
